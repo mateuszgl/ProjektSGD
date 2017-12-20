08637460a08b00bb5938534d16308ca033b43ae2
Mateusz Glodowski <mateusz95gl@gmail.com>
2017-12-20 11:47:13 -0800

    Changed texture of rocket to helicopter, added particles

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <thread>
+#include <vector>
 
 const int window_width = 640;
 const int window_height = 480;
-const int rocket_width = 32*2;
-const int rocket_height = 64*2;
+const int helicopter_width = 128*0.75;
+const int helicopter_height = 64*0.75;
 const int walls_number = 2;
+const auto particle_lifespan = std::chrono::milliseconds(100);
+
+struct Particle{
+	double x;
+	double y;
+	std::chrono::milliseconds lifespan;
+	int alfa;
+};
 
 auto errthrow = []( const std::string &e ) {
 	std::string errstr = e + " : " + SDL_GetError();
@@ -64,8 +73,8 @@ void draw_walls(std::shared_ptr<SDL_Renderer> renderer, SDL_Rect walls[]){
 }
 
 void draw_explosion(std::shared_ptr<SDL_Renderer> renderer, std::shared_ptr<SDL_Texture> texture, SDL_Rect rect, double x, double y){
-	rect.x = x-5;
-	rect.y = y+40;
+	rect.x = x + helicopter_width/2 - 36;
+	rect.y = y + helicopter_height/2 - 30;
 	SDL_RenderCopy( renderer.get(), texture.get(), NULL, &rect );
 }
 
@@ -110,29 +119,47 @@ void draw_fuel_gauge(std::shared_ptr<SDL_Renderer> renderer, double fuel, double
 
 }
 
-bool detect_collision_with_wall(SDL_Rect wall, double x, double y){
-	return ((x+rocket_width > wall.x + 5) 
-		&& (x < wall.x + wall.w - 5) 
-		&& (y + rocket_height > wall.y + 5)
-		&& (y < wall.y + wall.h - 5));
+void draw_particle(Particle part, std::shared_ptr<SDL_Renderer> renderer)
+{
+	SDL_SetRenderDrawColor(renderer.get(), 180, 180, 180, part.alfa);
+
+	for (int w = 1; w < 4; w++) {
+		for (int h = 1; h < 4; h++) {
+			SDL_RenderDrawPoint(renderer.get(), part.x+ w, part.y + h);
+		}
+	}
+	SDL_RenderDrawPoint(renderer.get(), part.x + 2, part.y);
+	SDL_RenderDrawPoint(renderer.get(), part.x, part.y + 2);
+	SDL_RenderDrawPoint(renderer.get(), part.x + 2, part.y + 4);
+	SDL_RenderDrawPoint(renderer.get(), part.x + 4, part.y + 2);
+	
+	SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 255);
+
 }
 
-bool check_game_won(double x, double y){
-	return ((x > window_width/2) && (y == window_height-rocket_height));
+void draw_all_particles(std::vector<Particle> particles, std::shared_ptr<SDL_Renderer> renderer){
+	for (unsigned int i = 0; i < particles.capacity(); i++){
+		draw_particle(particles.at(i), renderer);
+	}
+
+}
+
+bool detect_collision_with_wall(SDL_Rect wall, double x, double y){
+	return ((x+helicopter_width > wall.x + 5) 
+		&& (x < wall.x + wall.w) 
+		&& (y + helicopter_height > wall.y + 5)
+		&& (y < wall.y + wall.h));
 }
 
 void detect_collision(double &x, double &y, SDL_Rect walls[], double &dx, double &dy, double &ddx, double &ddy, bool &game_active){
 		
-	// collision with window
-	if ((x > window_width-rocket_width) || (x < 10) || (y < 0)){
+	if ((x > window_width-helicopter_width) || (x < 10) || (y < 0)){
 		game_active = false;
 	} else 
 
-	// landing
-	if(y > window_height-rocket_height) {
-		y = window_height-rocket_height;
+	if(y > window_height-helicopter_height) {
+		y = window_height-helicopter_height;
 	
-		// landing failed, too much speed
 		if(dy > 60){
 			game_active = false;
 		}
@@ -142,7 +169,6 @@ void detect_collision(double &x, double &y, SDL_Rect walls[], double &dx, double
 		ddy = 0; 
 
 	} else 
-	//collision with wall
 	{
 		for( unsigned int i = 0; i < walls_number; i++ ){	
 			if(detect_collision_with_wall(walls[i], x, y)){
@@ -152,16 +178,40 @@ void detect_collision(double &x, double &y, SDL_Rect walls[], double &dx, double
 	}
 }
 
-void calculate_steering(double &ddx, double &ddy, double &fuel, double dt){
+bool check_game_won(double x, double y){
+	return ((x > window_width/2) && (y == window_height-helicopter_height));
+}
+
+void calculate_particles(std::vector<Particle> &all_particles, double dt, double x, double y){
+	all_particles.at(0).lifespan -= std::chrono::milliseconds((int)(1000.0*dt));
+			
+	if(std::chrono::duration_cast<std::chrono::milliseconds>(all_particles.at(0).lifespan).count() < 0 ){
+		all_particles.erase(all_particles.begin());
+			
+		Particle particle;
+		particle.x = x+25;
+		particle.y = y+30;
+		particle.lifespan = particle_lifespan;
+		all_particles.push_back(particle);
+
+		all_particles.at(0).alfa = 55;
+		all_particles.at(1).alfa = 105;
+		all_particles.at(2).alfa = 155;
+		all_particles.at(3).alfa = 205;
+		all_particles.at(4).alfa = 255;
+	}
+
+}
+void calculate_acceleration(double &ddx, double &ddy, double &fuel, double dt){
 	
 	ddx = 0;
 	ddy = 100;
 	
 	const Uint8 *state = SDL_GetKeyboardState(NULL);
-	// every acceleration uses fuel
+
 	if (state[SDL_SCANCODE_UP] && fuel > 0) {
 		ddy = -150;
-		fuel -= dt;	
+		fuel -= dt;
 	}
 	if (state[SDL_SCANCODE_LEFT] && ddy < 0) {
 		ddx = -75;
@@ -181,11 +231,15 @@ void calculate_position(double &x, double &y, double &dx, double &dy, double ddx
 	
 }
 
+void update_position(SDL_Rect &helicopter, double x, double y){
+	helicopter.x = x;
+	helicopter.y = y;
+}
 
 
-int main( ) { // int argc, char **argv ) {
+int main( ) { 
 
-	double x = 75, y = window_height-rocket_height;    // starting position
+	double x = 75, y = window_height-helicopter_height;    // starting position
 	double dx = 0, dy = 0;     // speed
 	double ddx = 0, ddy = 0;   // acceleration 
 	double dt = 1.0/120.0;     // time increase
@@ -195,31 +249,32 @@ int main( ) { // int argc, char **argv ) {
 
 	auto window = init_window(window_width, window_height);
 	auto renderer = init_renderer( window );
-	// load textures
-	auto game_texture = load_texture( renderer, "data/rocket.bmp" );
+	SDL_SetRenderDrawBlendMode(renderer.get(), SDL_BLENDMODE_BLEND);
+
+	auto helicopter_texture = load_texture( renderer, "data/helicopter.bmp" );
 	auto explosion_texture = load_texture( renderer, "data/explosion.bmp" );
 	auto game_won_texture = load_texture( renderer, "data/game_won.bmp" );
 	auto game_over_texture = load_texture( renderer, "data/game_lose.bmp" );
 
-	// rectangle to display texture
-	SDL_Rect texr; 
-	texr.w = rocket_width;    //texture width
-	texr.h = rocket_height;   //texture height
+	SDL_Rect helicopter; 
+	helicopter.w = helicopter_width;    
+	helicopter.h = helicopter_height;   
+	helicopter.x = x;
+	helicopter.y = y;
 	
 	SDL_Rect explosion; 
-	explosion.w = 72; //texture width
-	explosion.h = 60; //texture height
+	explosion.w = 72; 
+	explosion.h = 60; 
 	
 	SDL_Rect game_status; 
-	game_status.w = 200; //texture width
-	game_status.h = 50; //texture height
+	game_status.w = 200;
+	game_status.h = 50;
 
 	SDL_Rect fuel_gauge; 
 	fuel_gauge.w = 10; 
 	fuel_gauge.x = 0;
 	fuel_gauge.y = 0;
 
-	// create some walls to hit
 	SDL_Rect wall; 
 	wall.w = 20;
 	wall.h = 200;
@@ -234,38 +289,38 @@ int main( ) { // int argc, char **argv ) {
 
 	SDL_Rect walls [] = {wall, wall2};
 
+	Particle particle;
+	particle.x = -10;
+	particle.y = -10;
+	particle.alfa = 255;
+	particle.lifespan = particle_lifespan;
+
+	std::vector<Particle> all_particles;
+	for(int i = 0; i< 5;i++){
+		all_particles.push_back(particle);
+	}
+
 	auto current_time = std::chrono::system_clock::now();
 	auto new_time = current_time;
 
-	//game loop
 	for ( bool game_active = true ; game_active; ) {
 
 		SDL_Event event;
-		// someone closed window event
 		while ( SDL_PollEvent( &event ) ) {
 			if ( event.type == SDL_QUIT ) game_active = false;
 		}
 
-		// detect collision with various objects
-		detect_collision(x, y, walls, dx, dy, ddx, ddy, game_active);
 
-		//update rocket position
-		texr.x = x;
-		texr.y = y;
 
-		//draw rocket in new position
 		SDL_RenderClear( renderer.get() );
-
-		// draw some walls
 		draw_walls(renderer, walls);
-
-		// draw rocket
-		SDL_RenderCopy( renderer.get(), game_texture.get(), NULL, &texr );
-
-		//draw fuel gauge
+		SDL_RenderCopy( renderer.get(), helicopter_texture.get(), NULL, &helicopter );
+		draw_all_particles(all_particles, renderer);
 		draw_fuel_gauge(renderer, fuel, starting_fuel, fuel_gauge);
 
-		// landing failed
+
+
+		detect_collision(x, y, walls, dx, dy, ddx, ddy, game_active);
 		if(!game_active){
 			draw_explosion(renderer, explosion_texture, explosion, x, y);
 			draw_game_status(renderer, game_over_texture, game_status);
@@ -275,14 +330,20 @@ int main( ) { // int argc, char **argv ) {
 			game_active = false;
 		}
 
-		SDL_RenderPresent( renderer.get() );
-	
-		calculate_steering(ddx,ddy,fuel,dt);
+
+
+		calculate_particles(all_particles, dt, x, y);
+		calculate_acceleration(ddx,ddy,fuel,dt);
 		calculate_position(x, y, dx, dy, ddx, ddy, dt);
+		update_position(helicopter, x, y);
+
+
 
 		new_time = current_time + std::chrono::milliseconds((int)(1000.0*dt));
 		std::this_thread::sleep_until (new_time);
 		current_time = new_time;
+		SDL_RenderPresent( renderer.get() );
+
 	}
 
 	std::this_thread::sleep_for (std::chrono::milliseconds(10000));
